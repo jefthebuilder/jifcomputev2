f3cfb9195da1bd822265dcb1d9cd5729ce92005b
jefthebuilder <162872160+jefthebuilder@users.noreply.github.com>
2024-06-05 20:17:53 +0200

    Update ramcontroller.v

--- src/ramcontroller.v
+++ src/ramcontroller.v
@@ -1,16 +1,31 @@
 module ramcontroller (
-  inout [15:0] ramdatain1,
-  inout [15:0] ramdatain2,
-  output [16:0] addressram1,
-  output [16:0] addressram2,
+  // ram connections
+  inout [7:0] ramdatain1,
+  output reg [1:0] bank,
+  output reg [1:0] bankgroup,
+  output reg active,
+  output reg [18:0] addressram1,
   output clockenable,
   output ramclock,
-  output cs1,
-  output cs2,
-  inout dqs1,
-  inout dqs2,
-  output reset,
-  
+  output reg cs1,
+  output reg refresh,
+  output reg reset,
+  // cpu controller connections
+  inout [63:0] data,
+  input [63:0] address,
+  input read,
+  input write,
+  input clock
+  output done_n,
+  input resetin
 );
-  
+  // kan zijn dat je refresh pin nodig hebt
+  reg state;
+  case (state) begin
+      0: begin
+        
+      end
+    
+  end
+    
 endmodule
