f3c6d6a943844c27504936873adb515f9dd10ddc
jefthebuilder <162872160+jefthebuilder@users.noreply.github.com>
2024-06-05 13:39:28 +0200

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,5 +1,7 @@
 `default_nettype none
 `timescale 1ns / 1ps
+`include "../src/alu.v"
+`include "../src/cpu.v"
 
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
